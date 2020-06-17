d42a3d48ac8ef9e632d2d435890dc7ead1cf5a4a
Shirin Muataz <shirinmuataz@gmail.com>
2020-06-17 00:46:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,9 +32,3 @@ function actionApplyer(startInt,arr){
   }
 }
 
-let Calculator ={
-  add:function(x,y){return x+y;},
-  subtract: function(x,y){return x-y;},
-  multiply: function(x,y){return x*y;},
-  divide: function(x,y){return x/y;}
-}
